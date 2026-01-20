040ee3d3f9a22ddd4ba58568363628307274ba2c
omartrese <108763869+omartrese@users.noreply.github.com>
2026-01-20 22:19:18 +0100

    fuck this shi am out

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+float suma(float a, float b)
+{
+	return a + b;
+}
+
 int main()
 {
-	printf("Hello madafucka\n");
+
+	printf("Prueba de 4 + 5: %f\n", suma(4, 5));
 	return 0;
 }
