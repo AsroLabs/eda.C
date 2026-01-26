1432f5a90822be8a79923ace316112bdea2dd280
omartrese <108763869+omartrese@users.noreply.github.com>
2026-01-26 20:43:13 +0100

    isEmpty() implementation

--- lib/stkque.c
+++ lib/stkque.c
@@ -1,25 +1,29 @@
 #include "stkque.h"
 
-Stack NewStack()
-{
-    Stack newStack; 
-    newStack.values = NULL;
-    newStack.size = 0;
-    newStack.peek = 0; 
-    return newStack; 
+Stack NewStack() {
+  Stack newStack;
+  newStack.values = NULL;
+  newStack.size = 0;
+  newStack.peek = 0;
+  return newStack;
 }
 
-void Push(Stack *stack, float value)
-{
-    stack->size++;
-    stack->values = realloc(stack->values, sizeof(float) * stack->size);
-    stack->values[stack->size - 1] = value;
-    stack->peek = value;
+void Push(Stack *stack, float value) {
+  stack->size++;
+  stack->values = realloc(stack->values, sizeof(float) * stack->size);
+  stack->values[stack->size - 1] = value;
+  stack->peek = value;
 }
 
-void Pop(Stack *stack)
-{
-    stack->size--;
-    stack->values = realloc(stack->values, sizeof(float) * stack->size);
+void Pop(Stack *stack) {
+  if (stack->size == 0)
+    return;
+
+  stack->size--;
+  stack->values = realloc(stack->values, sizeof(float) * stack->size);
+
+  if (stack->size > 0)
     stack->peek = stack->values[stack->size - 1];
 }
+
+bool isEmpty(Stack stack) { return stack.size <= 0; }
